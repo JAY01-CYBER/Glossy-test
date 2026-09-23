e612698ba54c30069f3bf463122fa5799152f563
Jay Chaudhary <82433052+JAY01-CYBER@users.noreply.github.com>
2026-09-23 20:25:00 +0530

    Delete spotifycore directory

--- spotifycore/src/main/kotlin /com/jay/glossy/spotifycore/models/SpotifyRecommendations.kt
+++
@@ -1,8 +0,0 @@
-package com.jay.glossy.spotifycore.models
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class SpotifyRecommendations(
-    val tracks: List<SpotifyTrack> = emptyList()
-)
